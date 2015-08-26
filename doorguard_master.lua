b4195d8c2a310edf32d53462f80094df1e9fa7b7
Arne Schauf <git@as-webservices.de>
2015-08-26 23:50:43 +0200

    v0.1

--- doorguard_master.lua
+++ doorguard_master.lua
@@ -1,24 +1,61 @@
 local config = require('config')
 local slaves = {"192.168.178.45"}
 
+local function showResult(good, bad)
+    if bad > 0 then
+        print("some bad! => "..bad)
+        print("good: "..good)
+        if config.PIN_RGB_LED then
+            ws2812.write(config.PIN_RGB_LED, string.char(0, 50, 0))
+        end
+    else
+        print("all good! => "..good)
+        print("bad: "..bad)
+        if config.PIN_RGB_LED then
+            ws2812.write(config.PIN_RGB_LED, string.char(50, 0, 0))
+        end
+    end
+    if config.PIN_RGB_LED then
+        tmr.alarm(1, 1000, 0, function ()
+            ws2812.write(config.PIN_RGB_LED, string.char(0, 0, 0))
+        end)
+    end
+end
+
 local function getStates()
+    local bad = 0
+    local good = 0
+    local received = 0
+    local expReceived = 0
+    local allScheduled = false
+    if config.PIN_RGB_LED then
+        ws2812.write(config.PIN_RGB_LED, string.char(50, 50, 0))
+    end
     for i, ip in ipairs(slaves) do
         local sock = net.createConnection(net.TCP, 0)
         sock:on("receive", function(sck, c)
-            print("states: ")
+            print("states: "..c)
             for i, v in ipairs(cjson.decode(c)["states"]) do
-                if v == 1 then
+                if v == 0 then
                     print(i.." => ok")
+                    good = good + 1
                 else
                     print(i.." => bad")
+                    bad = bad + 1
                 end
             end
             sck:close()
+            received = received + 1
+            if received == expReceived and allScheduled then
+                showResult(good, bad)
+            end
         end)
         print("Connecting to "..ip)
+        expReceived = expReceived + 1
         sock:connect(12345, ip)
         sock:send("x")
     end
+    allScheduled = true
 end
 
 local function start()
--- doorguard_slave.lua
+++ doorguard_slave.lua
@@ -1,9 +1,16 @@
 local config = require('config')
 
 local function start()
+    for i, pin in ipairs(config.PINS_REEDSWITCH) do
+        gpio.mode(pin, gpio.INPUT, gpio.PULLUP)
+    end
     local srv = net.createServer(net.TCP, 30)
     srv:listen(12345, function(sock)
-        sock:send(cjson.encode({states={0, 1}}))
+        states = {}
+        for i, pin in ipairs(config.PINS_REEDSWITCH) do
+            table.insert(states, gpio.read(pin))
+        end
+        sock:send(cjson.encode({states=states}))
     end)
 end
 
